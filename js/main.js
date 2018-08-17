bf3357141835c84d3bdff8f56ddde988b932eecd
James Mayol <jmayol@yahoo.com>
2018-08-17 10:27:13 -0400

    updated responsive test menu

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Triggers animation to non-bootstrap version of responsive menu
+function burgerFlip(x) {
+    x.classList.toggle("flipIt");
+}
+
+// Used for non-bootstrap version of resposive menu
+function toogleBurgerMenu(x) {
+    var x = document.getElementById("nonBootNav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        burgerFlip(x);
+    } else {
+        x.className = "topnav";
+    }
+}
